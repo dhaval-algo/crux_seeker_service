1d0f54535adcec70077f63f398532bf3e8dbb025
Latesh <latesh@ajency.in>
2020-09-14 12:48:34 +0530

    courses api

--- src/routes/apis/v1.js
+++ src/routes/apis/v1.js
@@ -1,6 +1,7 @@
 const userController = require('../../controllers/v1/apis/user');
-
+const courseController = require('../../controllers/v1/apis/courses');
 const express = require('express');
 let router = express.Router();
 router.use('/users', userController);
+router.use('/courses',courseController);
 module.exports = router;
