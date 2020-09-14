406a05e12411270307d08dd695d10b8613e026b2
Latesh <latesh@ajency.in>
2020-09-14 12:48:45 +0530

    minor fixes in middleware

--- src/services/v1/middleware/authenticate.js
+++ src/services/v1/middleware/authenticate.js
@@ -3,17 +3,17 @@ const DEFAULT_CODES = require("../../../utils/defaultCode").DEFAULT_CODES;
 
 module.exports  = (req, res, next) => {
     const authHeader = req.headers.authorization;
-    const audience = req.headers.host;
+    const audience = req.headers.origin;
     let options = {
         issuer: process.env.HOST,
         audience: audience,
-        algorithm:  "RS256",
+        algorithm:  ["RS256"],
     }
     if (authHeader) {
         const token = authHeader.split(' ')[1];
         const verifiedToken = require("../auth/auth").verifyToken(token, options);
         if(verifiedToken) {
-            req.user = verifyToken.user
+            req.user = verifiedToken.user
             next();
         } else {
             return res.status(200).send({
