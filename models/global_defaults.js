0606b35178c538e6cae49c76e9f37fa4cccff63e
Latesh <latesh@ajency.in>
2020-10-22 15:24:38 +0530

    complete crm inegratioon

--- models/global_defaults.js
+++ models/global_defaults.js
@@ -1,9 +1,9 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const global_defaults = sequelize.define('global_defaults', {
-    data_type: DataTypes.STRING,
-    data_value: DataTypes.STRING,
-    meta_data: DataTypes.TEXT
+    dataType: DataTypes.STRING,
+    dataValue: DataTypes.STRING,
+    metaData: DataTypes.TEXT
   }, {});
   global_defaults.associate = function(models) {
     // associations can be defined here
--- src/utils/subscriber.js
+++ src/utils/subscriber.js
@@ -1,4 +1,5 @@
 const EventEmitter = require('events');
+const { createLead } = require('../services/v1/zohoService');
 class Emitter extends EventEmitter {}
 const eventEmitter = new Emitter();
 
@@ -7,5 +8,9 @@ eventEmitter.on('error', () => {
     console.log('error')
 })
 
+eventEmitter.on('enquiry_placed', (enquiryId) => {
+    createLead(enquiryId)
+    
+})
 
 module.exports = eventEmitter
