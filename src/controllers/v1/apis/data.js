a4125fb1c0d84699705c255afaf96b5de41afc9e
Latesh <latesh@ajency.in>
2020-10-23 16:09:36 +0530

    insert func

--- src/controllers/v1/apis/data.js
+++ src/controllers/v1/apis/data.js
@@ -4,5 +4,6 @@ const dataService = require("../../../services/v1/data/data");
 const router = express.Router()
 
 router.get('/fetch-suggestion', dataService.fetchSuggestions)
+router.get('/insert', dataService.insertDegree)
 
 module.exports = router
