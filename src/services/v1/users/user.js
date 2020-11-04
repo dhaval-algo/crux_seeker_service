c2d81ce64e8c623536a79ed6258fa3d06719efcd
Latesh <latesh@ajency.in>
2020-11-04 18:58:05 +0530

    fix pagination

--- src/services/v1/users/user.js
+++ src/services/v1/users/user.js
@@ -985,15 +985,21 @@ const wishListCourseData = async (req,res) => {
 // fetch list of the enquires
 const getEnquiryList = async (req,res) => {
     try {
-        let { limit=5, page=0 } = req.query;
+        let { limit=5, page=1 } = req.query;
     const { user } = req;
-    let offset = page * limit
+    let offset = 0
+
+    if(page>1) {
+        offset =  (page-1)* limit
+    }
+    console.log("offset ", offset, page);
     const count = await models.form_submission.findAll({
 	
         attributes: ['userId', [sequelize.fn('count', sequelize.col('userId')), 'count']],
         where:{
             userId:user.userId || user.id,
-            targetEntityType:"course"
+            targetEntityType:"course",
+            status:'submitted'
         },
         group : ['userId'],
         
@@ -1003,15 +1009,16 @@ const getEnquiryList = async (req,res) => {
         
       });
       //fetch enquiries
-      let   where = { userId:user.userId || user.id, targetEntityType:"course"}
-      if(offset) {
-          where['offset'] = offset
+      let formSubConfig = { 
+        attributes: ['targetEntityId','otherInfo','createdAt'],
+        where: { userId:user.userId || user.id, targetEntityType:"course",status:'submitted'},
+        limit
       }
-      let enquiryRecs = await models.form_submission.findAll({ 
-          attributes: ['targetEntityId','otherInfo','createdAt'],
-          where :{ userId:user.userId || user.id, targetEntityType:"course",status:'submitted'},
-          limit
-        })
+     
+      if(page>1) {
+        formSubConfig.offset = offset
+      }
+      let enquiryRecs = await models.form_submission.findAll(formSubConfig)
         // no enquiries return
     if(!enquiryRecs.length) {
         return res.status(200).send({
@@ -1038,16 +1045,16 @@ const getEnquiryList = async (req,res) => {
               },
             }
         };
-        const result = await elasticService.plainSearch('learn-content', queryBody);
-        if(result.hits){
-            console.log(result.hits.hits.length,'-------------------------------');
-            if(result.hits.hits && result.hits.hits.length > 0){
-                for(const hit of result.hits.hits){
-                    enquiry.courseName = hit._source.title
-                    enquiry.categoryName = hit._source.categories? hit._source.categories.toString():""
-                }
-            }
-        }
+        // const result = await elasticService.plainSearch('learn-content', queryBody);
+        // if(result.hits){
+        //     console.log(result.hits.hits.length,'-------------------------------');
+        //     if(result.hits.hits && result.hits.hits.length > 0){
+        //         for(const hit of result.hits.hits){
+        //             enquiry.courseName = hit._source.title
+        //             enquiry.categoryName = hit._source.categories? hit._source.categories.toString():""
+        //         }
+        //     }
+        // }
         enquiriesDone.push(enquiry);
     }
     //fetch course fron esatic
