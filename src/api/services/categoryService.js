316ce0953e8012746a41d789eceaa062c8640e08
Robiul Hoque <robiul@AJ-Robiul.local>
2020-11-13 10:45:51 +0530

    fixed others naming issue

--- src/api/services/categoryService.js
+++ src/api/services/categoryService.js
@@ -8,11 +8,71 @@ const getCategoryTree = async () => {
     if (response.ok) {
         let json = await response.json();
         if(json && json.final_tree){
-            category_tree = json.final_tree;
+            //category_tree = json.final_tree;
+            category_tree = parseCategoryTree(json.final_tree);
         }
     }
     return category_tree;
 };
+
+
+const parseCategoryTree = (categoryTree) => {
+    let categories = [];
+    let other_categories = null;
+    for(const category of categoryTree){
+        if(category.count == 0){
+            continue;
+        }
+        let sub_categories = [];
+        let other_sub_categories = null;
+        for(const sub_category of category.child){
+            if(sub_category.count == 0){
+                continue;
+            }     
+
+            let topics = [];
+            let other_topics = null;
+            for(const topic of sub_category.child){
+                if(topic.count == 0){
+                    continue;
+                }
+
+                if(topic.slug == 'others'){
+                    other_topics = topic;
+                }else{
+                    topics.push(topic);
+                }
+            }
+            if(other_topics){
+                topics.push(other_topics);
+            }
+            sub_category.child = topics;
+            
+            if(sub_category.slug == 'others'){
+                other_sub_categories = sub_category;
+            }else{
+                sub_categories.push(sub_category);
+            }
+
+        }
+        if(other_sub_categories){
+            sub_categories.push(other_sub_categories);
+        }
+        category.child = sub_categories;
+
+        if(category.slug == 'others'){
+            other_categories = category;
+        }else{
+            categories.push(category);
+        }
+    }
+    if(other_categories){
+        categories.push(other_categories);
+    }
+    return categories;
+};
+
+
 
 module.exports = class categoryService {
 
