6c93a2b3b247daabaffe14deb07863ac5851e169
Latesh <latesh@ajency.in>
2020-11-23 15:51:04 +0530

    fix courses site map

--- src/services/v1/sitemap/index.js
+++ src/services/v1/sitemap/index.js
@@ -2,46 +2,53 @@ const SitemapStream = require('sitemap').SitemapStream
 const streamToPromise = require('sitemap').streamToPromise
 const { default: Axios } = require('axios');
 const { uploadFileToS3 } = require('../AWS');
-
-const iterate = (obj,smStream, route) => {
+let slugs = []
+const iterate = (obj, smStream, route) => {
     Object.keys(obj).forEach(key => {
 
-    // console.log('key: '+ key + ', value: '+obj[key]);
-    switch (route) {
-        case "courses":
-            
-            switch (obj[key]) {
-                case 'category':
-                case 'sub-category':
-                    smStream.write({
-                        url: `/courses/${obj.slug}`,
-                    })
-                    break;
-                default:
-                    break;
-            }
-            break;
-        case "topic":
-            switch (obj[key]) {
-                case 'topic':
-                    smStream.write({
-                        url: `/topic/${obj.slug}`,
-                    })
-                    break; 
-                default:
-                    break;
-            }
-            break;
-        
-        default:
-            break;
-    }
-    if (typeof obj[key] === 'object') {
+        switch (route) {
+            case "courses":
+                switch (obj[key]) {
+                    case 'category':
+                    case 'sub-category':
+                            // if (slugs.includes(obj.slug)){
+                            //     console.log(obj.slug, "includesss");
+                            //     break;
+                            // }
+                        smStream.write({
+                            url: `/courses/${obj.slug}`,
+                        })
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case "topic":
+                // if (slugs.includes(obj.slug))
+                //     break;
+                switch (obj[key]) {
+                    case 'topic':
+                        smStream.write({
+                            url: `/topic/${obj.slug}`,
+                        })
+                        break;
+                    default:
+                        break;
+                }
+                break;
 
-            iterate(obj[key],smStream,route)
+            default:
+                break;
+        }
+        // if(!slugs.includes(obj.slug)){
+        //     slugs.push(obj.slug)
+        // }
+        if (typeof obj[key] === 'object') {
+            iterate(obj[key], smStream, route)
         }
     })
     return smStream;
+
 }
 
 function createCourse() {
@@ -62,24 +69,23 @@ function createCategories() {
             smStream.write({
                 url: "/about",
             });
-           
+
             // fetch category tree
             const backEndUrl = `${process.env.API_BACKEND_URL}/category-tree`;
             const categoryTreeRes = await Axios.get('https://crux-backend.ajency.in/category-tree')
-            let {final_tree} = categoryTreeRes.data
+            let { final_tree } = categoryTreeRes.data
             smStream = iterate(final_tree, smStream, "courses")
-            
+
             //generate course url 
 
             smStream.end();
             // generate a sitemap and add the XML feed to a url which will be used later on.
             const sitemap = await streamToPromise(smStream).then((sm) => sm.toString());
             //write to aws 
-            let path ='courses.xml'
-            let contentType= 'text/xml'
+            let path = 'courses.xml'
+            let contentType = 'text/xml'
             const res = await uploadFileToS3(path, sitemap, contentType)
-            console.log('ccategory', res);
-            resolve(true)
+            resolve(sitemap)
         } catch (error) {
             console.log(error);
 
@@ -88,29 +94,28 @@ function createCategories() {
     })
 }
 
-function createTopic(obj,smsStream) {
-    return new Promise( async resolve => {
+function createTopic(obj, smsStream) {
+    return new Promise(async resolve => {
         try {
             let smStream = new SitemapStream({
                 hostname: process.env.FRONTEND_URL,
             });
-          
+
             // fetch category tree
             const backEndUrl = `${process.env.API_BACKEND_URL}/category-tree`;
             const categoryTreeRes = await Axios.get('https://crux-backend.ajency.in/category-tree')
-            let {final_tree} = categoryTreeRes.data
+            let { final_tree } = categoryTreeRes.data
             smStream = iterate(final_tree, smStream, "topic")
-            
+
             //generate course url 
 
             smStream.end();
             // generate a sitemap and add the XML feed to a url which will be used later on.
             const sitemap = await streamToPromise(smStream).then((sm) => sm.toString());
             //write to aws 
-            let path ='topic.xml'
-            let contentType= 'text/xml'
+            let path = 'topic.xml'
+            let contentType = 'text/xml'
             const res = await uploadFileToS3(path, sitemap, contentType)
-            console.log("topic",res);
             resolve(true)
         } catch (error) {
             console.log(error);
@@ -122,15 +127,17 @@ module.exports = {
     createSiteMap: () => {
         return new Promise(async (resolve) => {
             try {
-                
-                await createCategories()
+                slugs = []
+                const result = await createCategories()
+                slugs = [] // before calling clear sllugs array 
                 await createTopic()
+                slugs = []
                 await createCourse()
-                resolve(true)
+                resolve(result)
             } catch (error) {
                 console.log(error);
-                resolve(true)
-                
+                resolve(false)
+
             }
         })
     }
