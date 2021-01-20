416ae3bd2443a2b3b0147a9334184f834e53c347
Sharang Sinai Amonkar <sharang@ajency.in>
2021-01-20 18:16:55 +0530

    added the token authentication

--- src/api/routes/auth/v1/index.js
+++ src/api/routes/auth/v1/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const learnContentController = require("../../../controllers/learnContentController");
+const authenticateJWT = require("../../../../services/v1/middleware/authenticate");
 
-router.get("/course/buy", learnContentController.buyCourse);
+router.get("/course/buy", authenticateJWT, learnContentController.buyCourse);
 
 module.exports = router;
