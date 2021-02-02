b25e64f9e10ff07507c94ceb910dd584a3d176f0
Sharang Sinai Amonkar <sharang@ajency.in>
2021-02-02 08:05:30 +0530

    added status polling api

--- src/api/routes/auth/v1/index.js
+++ src/api/routes/auth/v1/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const learnContentController = require("../../../controllers/learnContentController");
+const orderController = require("../../../controllers/orderController");
 const authenticateJWT = require("../../../../services/v1/middleware/authenticate");
 
 router.post("/course/buy", authenticateJWT, learnContentController.buyCourse);
+router.get("/order/status", authenticateJWT, orderController.getOrderStatus);
 
 module.exports = router;
