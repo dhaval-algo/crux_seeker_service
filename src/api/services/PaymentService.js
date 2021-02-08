0caa7a761e9fafa8de117ed37faad136902bd5d0
Sharang Sinai Amonkar <sharang@ajency.in>
2021-02-08 08:00:25 +0530

    added customer to the payment intent when required

--- src/api/services/PaymentService.js
+++ src/api/services/PaymentService.js
@@ -2,23 +2,33 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 module.exports = class PaymentService {
     async createPaymentIntent(amount, currency, description, userObj) {
-        let paymentIntent = await stripe.paymentIntents.create({
+        /** Add the customer */
+        let customer = null;
+        if(currency !== "INR") {
+            customer = await stripe.customers.create({
+                name: userObj.firstName + " " + userObj.lastName,
+                address: {
+                    line1: 'Goa',
+                    postal_code: '403001',
+                    city: 'Panaji',
+                    state: 'GOA',
+                    country: 'IN',
+                }
+            });
+        }
+        
+        /** Create the payment intent */
+        let paymentIntentPayload = {
             amount: amount * 100,
             currency: currency,
             description: description,
-        });
+        }
 
-        /** Add the customer */
-        await stripe.customers.create({
-            name: userObj.firstName + " " + userObj.lastName,
-            address: {
-                line1: 'Goa',
-                postal_code: '403001',
-                city: 'Panaji',
-                state: 'GOA',
-                country: 'IN',
-            }
-        });
+        if(customer) {
+            paymentIntentPayload.customer = customer.id;
+        }
+
+        let paymentIntent = await stripe.paymentIntents.create(paymentIntentPayload);
 
         if(paymentIntent) {
             return paymentIntent.client_secret;
