8e874fcd500b0e8a2a2d268142d5ef88ac9d9cec
Rahul <rahul@ajency.in>
2021-04-29 12:34:03 +0530

    test on issue 861

--- src/services/v1/users/user.js
+++ src/services/v1/users/user.js
@@ -1234,15 +1234,18 @@ const uploadSkills = async (req,res) => {
 }
 
 const fetchUserMetaObjByUserId = async (id) => {
-    let where = {
-        userId: id,
-        key: 'firstName',
-        metaType: 'primary'
-      };
-      let obj = await models.user_meta.findOne({where});
-      let firstname = obj.dataValues.value;
-      
-      return firstname;
+    let userData = {};
+    let keys = ['firstName','lastName','email','phone'];
+    for(key of keys){
+        let where = {
+            userId: id,
+            key: key,
+            metaType: 'primary'
+        };
+        let obj = await models.user_meta.findOne({where});
+        userData[key] = obj.dataValues.value;
+    }
+    return userData;
 }
 
 
