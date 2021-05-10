10a8925ba84c975497da6f3c557371d89eff9f31
Rahul <rahul@ajency.in>
2021-05-10 11:14:22 +0530

    filtered news data

--- src/api/services/newsService.js
+++ src/api/services/newsService.js
@@ -1,5 +1,13 @@
 const elasticService = require("./elasticService");
 
+const getNewsData = async (data) => {
+    let newData = [];
+    for(let i=0;i<data.length;i++){
+        newData.push(data[i]._source);
+    }
+    return newData;
+}
+
 module.exports = class CustomPageService {
     
     async getNewsContent(slug, callback){
@@ -16,7 +24,8 @@ module.exports = class CustomPageService {
             console.log('Error while retriving data',e);
         }
         if(result && result.hits && result.hits.length > 0) {
-            callback(null, {status: 'success', message: 'Fetched successfully!', data:result.hits});
+            let newsData = await getNewsData(result.hits);
+            callback(null, {status: 'success', message: 'Fetched successfully!', data:newsData});
         } else {
             callback(null, {status: 'failed', message: 'No data available!', data: []});
         }
