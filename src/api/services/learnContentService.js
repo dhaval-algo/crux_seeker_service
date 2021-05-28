eb8ab070cc74c9714e7c9bd4f740d393c1a1c8da
Prajay Verenkar <prajay@ajency.in>
2021-05-28 16:38:51 +0530

    test emdium

--- src/api/services/learnContentService.js
+++ src/api/services/learnContentService.js
@@ -831,7 +831,7 @@ module.exports = class learnContentService {
                 {
                     "query_string" : {
                         "query" : `*${decodeURIComponent(req.query['q'])}*`,
-                        "fields" : ['title','categories','sub_categories','provider_name','level','learnng_mediums','partner_name'],
+                        "fields" : ['title','categories','sub_categories','provider_name','level','medium','partner_name'],
                         "analyze_wildcard" : true,
                         "allow_leading_wildcard": true
                     }
