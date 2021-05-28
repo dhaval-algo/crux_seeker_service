0468717755ec258bae849e87af166430996ddaec
Rahul <rahul@ajency.in>
2021-05-28 15:06:39 +0530

    added verification email

--- src/communication/v1/template.js
+++ src/communication/v1/template.js
@@ -7,7 +7,7 @@ module.exports = {
 
  
     activiationLink: function(messagData){
-        let templatesPath = path.join(__dirname, './templates/verification-email.hbs');
+        let templatesPath = path.join(__dirname, './templates/verifyemail2.hbs');
 
         let source = fs.readFileSync(templatesPath, 'utf8');
         let template = handlebars.compile(source);
