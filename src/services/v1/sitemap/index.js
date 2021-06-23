5c36a61839e8fe24ea5961824bc97e607530c673
Sudhir <sudhir@ajency.in>
2021-06-23 12:17:17 +0530

    correction in path

--- src/services/v1/sitemap/index.js
+++ src/services/v1/sitemap/index.js
@@ -535,7 +535,7 @@ module.exports = {
     copySiteMapS3ToFolder: () => {
         try {
             console.log("into the fucntion");
-            exec('aws s3 cp s3://crux-assets-production/advice.xml sitemapfiles/advice.xml') 
+            exec('aws s3 cp s3://crux-assets-production/advice.xml /sitemapfiles/advice.xml') 
             console.log("Site map file  copied");
         } catch (error) {
             console.log("Shellerorr===========",error )
