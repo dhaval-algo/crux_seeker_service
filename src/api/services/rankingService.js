d02e3cb99da8c9d5116ef6dfb8123054fced2a24
Viraj Verlekar <viraj@ajency.in>
2021-07-08 17:46:58 +0530

    redis setup + ranking

--- src/api/services/rankingService.js
+++ src/api/services/rankingService.js
@@ -1,34 +1,61 @@
 const elasticService = require("./elasticService");
 const articleService = require('./articleService');
+const redisConnection = require('../../services/v1/redis');
 
 const ArticleService = new articleService();
+const RedisConnection = new redisConnection();
 
-const formatHomepageData = async(data) => {    
+const formatHomepageData = async(data) => {
+    let articles = [];
     if(data.top_articles.length){
         data.top_articles = await ArticleService.getArticleByIds(data.top_articles, false);
+        data.top_articles.forEach(obj => {
+            articles.push(obj.slug);
+        });
     }
     if(data.trending_articles.length){
         data.trending_articles = await ArticleService.getArticleByIds(data.trending_articles);
+        data.trending_articles.forEach(obj => {
+            articles.push(obj.slug);
+        });
     }
     if(data.featured_articles.length){
         data.featured_articles = await ArticleService.getArticleByIds(data.featured_articles, false);
+        data.featured_articles.forEach(obj => {
+            articles.push(obj.slug);
+        });
     }
     if(data.online_tech_articles.length){
         data.online_tech_articles = await ArticleService.getArticleByIds(data.online_tech_articles);
+        data.online_tech_articles.forEach(obj => {
+            articles.push(obj.slug);
+        });
     }
     if(data.online_non_tech_articles.length){
         data.online_non_tech_articles = await ArticleService.getArticleByIds(data.online_non_tech_articles);
+        data.online_non_tech_articles.forEach(obj => {
+            articles.push(obj.slug);
+        });
     }
     if(data.executive_education.length){
         data.executive_education = await ArticleService.getArticleByIds(data.executive_education);
+        data.executive_education.forEach(obj => {
+            articles.push(obj.slug);
+        });
     }
     if(data.mba_rankings.length){
         data.mba_rankings = await ArticleService.getArticleByIds(data.mba_rankings);
+        data.mba_rankings.forEach(obj => {
+            articles.push(obj.slug);
+        });
     }
     if(data.engineering_rankings.length){
         data.engineering_rankings = await ArticleService.getArticleByIds(data.engineering_rankings);
+        data.engineering_rankings.forEach(obj => {
+            articles.push(obj.slug);
+        });
     }
-    return data;
+    return {data:data, articles:articles};
 };
 
 module.exports = class rankingService {  
@@ -45,8 +72,10 @@ module.exports = class rankingService {
       };
       const result = await elasticService.search('ranking-home-page', query);
       if (result.hits && result.hits.length) {
-        data = await formatHomepageData(result.hits[0]._source)
-        return callback(null, { success: true, data })
+        let response = await formatHomepageData(result.hits[0]._source);
+        RedisConnection.set('ranking-article-slug', response.articles);
+        RedisConnection.set('ranking-page', response.data);
+        return callback(null, { success: true, data:response.data });
       }
       return callback(null, { success: true, data:data })
 
---
+++ src/services/v1/redis/index.js
@@ -0,0 +1,89 @@
+let client = require('redis');
+
+let config = {
+    url: process.env.REDIS_URL
+}
+
+let redis;
+
+module.exports = class REDIS {
+    connect() {
+        if (!redis) {
+            redis = client.createClient(config);
+        }
+    }
+
+    getAllKey(callback){
+        this.connect(); 
+        redis.keys('apiData-*',callback);
+    }
+
+    get(key,callback){
+        this.connect(); 
+        key='apiData-'+key;
+        redis.get(key,callback);
+    }
+
+    getValues(configKey,key,callback){
+        this.connect();
+        key='apiData-'+key;
+        redis.get(key,(err,data)=>{
+            if(err){
+                callback(err,null);
+            }
+            else{
+                if(data == null){
+                    console.log('REDIS:: No cache available for key = ',key);
+                    callback(null,{noCacheData:true})
+                }
+                else{
+                    console.log('REDIS:: Cache available for key = ',key);
+                    callback(null,data);
+                }
+            }
+        });
+    }
+
+    getValuesSync(configKey,key){
+        let that = this;
+        return new Promise(function(resolve,reject){
+            that.connect();
+            key='apiData-'+key;
+            redis.get(key,(err,data)=>{
+                if(err){
+                    resolve({noCacheData:true});
+                }
+                else{
+                    if(data == null){
+                        console.log('REDIS:: No cache available for key = ',key);
+                        resolve({noCacheData:true})
+                    }
+                    else{
+                        console.log('REDIS:: Cache available for key = ',key);
+                        resolve(data);
+                    }
+                }
+            }); 
+        })
+    }
+
+    async set(key,value){
+        this.connect();
+        key='apiData-'+key;
+        redis.set(key,JSON.stringify(value),function(err,response){
+            if(response){
+                console.log("Redis object added for key = ",key);
+            }
+            else{
+                console.log("Error while setting object for key = "+key+" error = "+err);
+            }
+        });
+    }
+
+    delete(key){
+        this.connect(); 
+        key='apiData-'+key;
+        console.log("CRON::Delete key for  "+key);
+        redis.del(key);
+    }
+}
