1684ec343385a0205cd31de4b97d860f469072cc
Sudhir <sudhir@ajency.in>
2021-07-22 17:56:56 +0530

    new institutes, degree, specialization, job titles, industry, company to be added #1366

--- src/controllers/v1/apis/data.js
+++ src/controllers/v1/apis/data.js
@@ -4,7 +4,7 @@ const dataService = require("../../../services/v1/data/data");
 const router = express.Router()
 
 router.get('/fetch-suggestion', dataService.fetchSuggestions)
-router.get('/insert', dataService.insertDegree)
+router.get('/insert/:slug', dataService.insertDefaultOption)
 router.get('/places-autocomplete', dataService.placesAutoComplete)
 
 module.exports = router
