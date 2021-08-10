4018b1f4a2e8c00ebd34be642731fc0b55110a13
Sudhir <sudhir@ajency.in>
2021-08-10 17:22:09 +0530

    logs

--- src/api/services/learnContentService.js
+++ src/api/services/learnContentService.js
@@ -697,6 +697,10 @@ module.exports = class learnContentService {
         let defaultSort = "published_date:desc";
         let useCache = false;
         let cacheName = "";
+        console.log("pageType=========================", req.query['pageType']);
+            console.log("q=========================", req.query['q']);
+            console.log("currency=========================", req.query['currency']);
+            console.log("skipCache=========================", skipCache);
         if(
             req.query['courseIds'] == undefined
             && req.query['f'] == undefined
@@ -716,11 +720,7 @@ module.exports = class learnContentService {
             let apiCurrency = process.env.DEFAULT_CURRENCY;
             if(req.query['currency'] != undefined){
                 apiCurrency = req.query['currency'];
-            }
-            console.log("pageType=========================", req.query['pageType']);
-            console.log("q=========================", req.query['q']);
-            console.log("currency=========================", req.query['currency']);
-            console.log("skipCache=========================", skipCache);
+            }            
             if((req.query['pageType'] == "category" || req.query['pageType'] == "topic") && req.query['slug'] != undefined && (req.query['q'] == undefined || req.query['q'] == "")) {
                 cacheName = "listing-"+req.query['pageType']+"-"+req.query['slug'].replace(/,/g, '_')+"_"+apiCurrency;
             } else if((req.query['pageType'] == undefined || req.query['pageType'] == "search") && (req.query['q'] == undefined || req.query['q'] == "")) {
