de455bbf43e90d87004c07f4ec85740cd06daaed
Sudhir <sudhir@ajency.in>
2021-08-12 19:07:48 +0530

    logs

--- src/services/v1/forms/enquirySubmission.js
+++ src/services/v1/forms/enquirySubmission.js
@@ -243,7 +243,11 @@ const handleUserProfileSubmission = (resBody, req) => {
 
         try {         
             await updateProfileMeta(formData, userObj)
+            console.log("user=====",user)
             const userinfo = await models.user_meta.findOne({where:{userId:user.userId, metaType:'primary', key:'email'}})
+            console.log("userinfo=====",userinfo)
+            console.log("userinfo.value=====",useriuserinfo.valuenfo)
+
             formData.email = userinfo.value
             sendDataForStrapi(formData, "update-user-profile")
             const progress = await calculateProfileCompletion(userObj)
