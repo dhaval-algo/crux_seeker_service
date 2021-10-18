f801a1cc783e1d0bc87d547a8a8b6e7ae0769553
Sudhir <sudhir@ajency.in>
2021-10-18 10:20:00 +0530

    Cache control header

--- src/api/controllers/learnContentController.js
+++ src/api/controllers/learnContentController.js
@@ -74,6 +74,10 @@ module.exports = {
     getPopularCourses: async (req, res)=>{
         LearnContentService.getPopularCourses(req,(err, data)=>{
             if (data) {
+                if(process.env.API_CACHE_CONTROL_HEADER)
+                {
+                    res.set('Cache-control', process.env.API_CACHE_CONTROL_HEADER)
+                }
                 res.status(200).send(data);
             } else {
                 res.status(200).send(err);
