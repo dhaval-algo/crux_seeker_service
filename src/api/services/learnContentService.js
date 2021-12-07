eb5ec47154e3173b264d804026f450f91b58b79c
Sudhir <sudhir@ajency.in>
2021-12-07 17:01:20 +0530

    Try catch block

--- src/api/services/learnContentService.js
+++ src/api/services/learnContentService.js
@@ -1144,43 +1144,49 @@ module.exports = class learnContentService {
 
 
     async getCourseByIds(req, callback){
-        if(currencies.length == 0){
-            currencies = await getCurrencies();
-        }
-        let courses = [];
-        let courseOrdered = [];
-        let ids = [];
-        if(req.query['ids']){
-            ids = req.query['ids'].split(",");
-        }
-        if(ids.length > 0){
-            const queryBody = {
-                "query": {
-                  "ids": {
-                      "values": ids
-                  }
-                }
-            };
-
-            const result = await elasticService.search('learn-content', queryBody);
-            if(result.hits){
-                if(result.hits && result.hits.length > 0){
-                    for(const hit of result.hits){
-                        const course = await this.generateSingleViewData(hit._source, false, req.query.currency);
-                        courses.push(course);
+        try {
+            if(currencies.length == 0){
+                currencies = await getCurrencies();
+            }
+            let courses = [];
+            let courseOrdered = [];
+            let ids = [];
+            if(req.query['ids']){
+                ids = req.query['ids'].split(",");
+            }
+            if(ids.length > 0){
+                const queryBody = {
+                    "query": {
+                      "ids": {
+                          "values": ids
+                      }
                     }
-                    for(const id of ids){
-                        let course = courses.find(o => o.id === id);
-                        courseOrdered.push(course);
+                };
+    
+                const result = await elasticService.search('learn-content', queryBody);
+                if(result.hits){
+                    if(result.hits && result.hits.length > 0){
+                        for(const hit of result.hits){
+                            const course = await this.generateSingleViewData(hit._source, false, req.query.currency);
+                            courses.push(course);
+                        }
+                        for(const id of ids){
+                            let course = courses.find(o => o.id === id);
+                            courseOrdered.push(course);
+                        }
                     }
-                }
-            }            
+                }            
+            }
+            if(callback){
+                callback(null, {status: 'success', message: 'Fetched successfully!', data: courseOrdered});
+            }else{
+                return courseOrdered;
+            }
+        } catch (error) {
+            callback(null, {status: 'error', message: 'Failed to Fetch', data: null});
+            console.log("course by id error=>",error)
         }
-        if(callback){
-            callback(null, {status: 'success', message: 'Fetched successfully!', data: courseOrdered});
-        }else{
-            return courseOrdered;
-        }
+        
         
     }
 
