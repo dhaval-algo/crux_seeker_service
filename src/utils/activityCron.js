16db1a7ff52d3873f4a71d05a3d0c90cd9f75d51
Saurabh Jindal <saurabh@careervira.com>
2021-12-28 15:50:34 +0530

    added activity for learn path

--- src/utils/activityCron.js
+++ src/utils/activityCron.js
@@ -4,6 +4,163 @@ const Op = Sequelize.Op;
 const { publishToSNS } = require('../services/v1/sns');
 const moment = require("moment");
 
+const learnpathActivity = async () => {
+    let activity_types = {}
+    let activity_count = []
+    const activities =  await models.activity.findAll({
+        attributes: ["id","type"],        
+        raw:true
+    })
+    
+    for (activity_type of activities)
+    {
+        activity_types[activity_type.id] = activity_type.type
+    }
+    
+    // All time counts for logged in user 
+    const activity_logs_all =  await models.activity_log.findAll({
+        attributes: [[Sequelize.fn('count', Sequelize.col('id')), "count"],"resource","activityId","createdAt"],         
+        group: ['activityId', "resource","createdAt"],
+        raw:true
+    })
+    
+    const activity_log_x_days = activity_logs_all.filter((activity) => (activity.createdAt > moment().subtract(process.env.LAST_X_DAYS_COUNT, 'days').toDate())) 
+    
+    for (let activity of activity_logs_all)
+    {
+        if(!activity_count[activity.resource])
+        {
+            activity_count[activity.resource] = {}
+            activity_count[activity.resource].all_time = {
+                learnpath_views:0,
+                learnpath_wishlists:0,
+                learnpath_enquiries:0,
+                learnpath_purchase:0
+            }
+            activity_count[activity.resource].last_x_days = {
+                learnpath_views:0,
+                learnpath_wishlists:0,
+                learnpath_enquiries:0,
+                learnpath_purchase:0
+            }
+        }
+        
+        switch (activity_types[activity.activityId]) {
+            case "LEARNPATH_VIEW": 
+                activity_count[activity.resource].all_time.learnpath_views= Number(activity.count)              
+                break;
+            case "LEARNPATH_WISHLIST":
+                activity_count[activity.resource].all_time.learnpath_wishlists= Number(activity.count)              
+                break;
+            case "LEARNPATH_ENQUIRED":
+                activity_count[activity.resource].all_time.learnpath_enquiries= Number(activity.count)
+                break;                
+            case "LEARNPATH_PURCHASED":
+                activity_count[activity.resource].all_time.learnpath_purchase= Number(activity.count)
+                break;
+            default:
+                break;
+        }  
+    }
+
+    for (let activity of activity_log_x_days)
+    {
+        switch (activity_types[activity.activityId]) {
+            case "LEARNPATH_VIEW": 
+                activity_count[activity.resource].last_x_days.learnpath_views= Number(activity.count)
+                break;
+            case "LEARNPATH_WISHLIST":
+                activity_count[activity.resource].last_x_days.learnpath_wishlists= Number(activity.count)
+                break;
+            case "LEARNPATH_ENQUIRED":
+                activity_count[activity.resource].last_x_days.learnpath_enquiries= Number(activity.count)
+                break;                
+            case "LEARNPATH_PURCHASED":
+                activity_count[activity.resource].last_x_days.learnpath_purchase= Number(activity.count)
+                break;
+            default:
+                break;
+        } 
+    }
+
+    // All time counts for non-logged in user 
+    const activity_logs_loggedout_all =  await models.activity_log_loggedout.findAll({
+        attributes: [[Sequelize.fn('count', Sequelize.col('id')), "count"],"resource","activityId","createdAt"],         
+        group: ['activityId', "resource", "createdAt"],
+        raw:true
+    })
+
+    const activity_logs_loggedout_x_days = activity_logs_loggedout_all.filter((activity) => (activity.createdAt > moment().subtract(process.env.LAST_X_DAYS_COUNT, 'days').toDate()))
+
+    for (let activity of activity_logs_loggedout_all)
+    {
+        if(!activity_count[activity.resource])
+        {
+            activity_count[activity.resource] = {}
+            activity_count[activity.resource].all_time = {
+                learnpath_views:0,
+                learnpath_wishlists:0,
+                learnpath_enquiries:0,
+                learnpath_purchase:0
+            }
+
+            activity_count[activity.resource].last_x_days = {
+                learnpath_views:0,
+                learnpath_wishlists:0,
+                learnpath_enquiries:0,
+                learnpath_purchase:0
+            }
+        }
+
+        switch (activity_types[activity.activityId]) {
+            case "LEARNPATH_VIEW": 
+                activity_count[activity.resource].all_time.learnpath_views += Number(activity.count)             
+                break;
+            case "LEARNPATH_WISHLIST":
+                activity_count[activity.resource].all_time.learnpath_wishlists += Number(activity.count)
+                break;
+            case "LEARNPATH_ENQUIRED":
+                activity_count[activity.resource].all_time.learnpath_enquiries += Number(activity.count)
+                break;                
+            case "LEARNPATH_PURCHASED":
+                activity_count[activity.resource].all_time.learnpath_purchase += Number(activity.count)
+                break;
+            default:
+                break;
+        }   
+    }
+
+    for (let activity of activity_logs_loggedout_x_days)
+    {
+        switch (activity_types[activity.activityId]) {
+            case "LEARNPATH_VIEW": 
+                activity_count[activity.resource].last_x_days.learnpath_views += Number(activity.count)
+                break;
+            case "LEARNPATH_WISHLIST":
+                activity_count[activity.resource].last_x_days.learnpath_wishlists += Number(activity.count)
+                break;
+            case "LEARNPATH_ENQUIRED":
+                activity_count[activity.resource].last_x_days.learnpath_enquiries += Number(activity.count)
+                break;                
+            case "LEARNPATH_PURCHASED":
+                activity_count[activity.resource].last_x_days.learnpath_purchase += Number(activity.count)
+                break;
+            default:
+                break;
+        }
+        
+    }
+
+    for ( const [key, value] of Object.entries(activity_count))
+    {
+        let payload = {
+            learn_path_id:key,
+            activity_count:value,
+        }
+        publishToSNS(process.env.LEARN_PATH_ACTIVITY_TOPIC_ARN, payload, "LEARNPATH_ACTIVITY_COUNT")
+    }
+}
+
 const storeActivity = async () => {
     let activity_types = {}
     let activity_count = []
@@ -187,5 +344,6 @@ const storeActivity = async () => {
 }
    
 module.exports = {
-    storeActivity
+    storeActivity,
+    learnpathActivity
 }
