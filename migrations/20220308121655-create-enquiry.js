4a0c59fed32b12494cf2f6faab92d2ace80142d3
Vinesh Raut <vinesh@ajency.in>
2022-03-11 17:43:12 +0530

    migrations: definition for course enquiry
    new table structure for enquiry data

---
+++ migrations/20220308121655-create-enquiry.js
@@ -0,0 +1,78 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('enquiries', {
+      id : {
+        type:Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+
+      fullName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+
+    email : {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+
+    phone : {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+
+    student : {
+        type:Sequelize.BOOLEAN
+    },
+
+    highestDegree : {
+        type: Sequelize.STRING,
+    },
+
+    experience : {
+        type: Sequelize.STRING,
+    },
+
+    enquiryMessage : {
+        type: Sequelize.STRING(512),
+    },
+    
+    courseName : {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+
+    courseId : {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+
+    userId : {
+        type: Sequelize.INTEGER,
+    },
+    
+    partnerId : {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+
+    },
+
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+      
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('enquiries');
+  }
+};
