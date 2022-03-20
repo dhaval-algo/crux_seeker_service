fddc9d0048f25945a89453aa56a8b05007ec38b0
Sudhir <sudhir@ajency.in>
2022-03-20 23:26:56 +0530

    import script for old data

--- migrations/20220315052919-user_add_full_name_email_phone.js
+++ migrations/20220315052919-user_add_full_name_email_phone.js
@@ -12,12 +12,10 @@ module.exports = {
       try {
         await queryInterface.addColumn('users', 'fullName', {
           type: Sequelize.STRING,
-          allowNull: false,
         });
         await queryInterface.addColumn('users', 'email', {
           type: Sequelize.STRING,
-          allowNull: false,
-          unique: true
+          unique: true,
         }).
         then(() => queryInterface.addIndex('users', ['email']));
         
