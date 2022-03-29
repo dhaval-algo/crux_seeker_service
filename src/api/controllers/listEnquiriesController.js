e6085bdaa3885a400e448213b1309cb230499ca5
Vinesh Raut <vinesh@ajency.in>
2022-03-29 09:56:46 +0530

    controller: definition: getDetailedEnquiry()
    this will fetch the enquiry based on (e)id parameter within url
    and returns all details of the enquiry based on eid

--- src/api/controllers/listEnquiriesController.js
+++ src/api/controllers/listEnquiriesController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const models = require("../../../models")
-const { buildConfig } = require("../../api/services/listEnquiriesService")
+const { buildConfig, getEnquiry} = require("../../api/services/listEnquiriesService")
 
 const list = async (req, res) =>{
     try{
@@ -18,6 +18,27 @@ const list = async (req, res) =>{
     }
 }
 
+const getDetailedEnquiry = async (req, res)=>{
+    try{
+        let {id} = req.params
+        id = parseInt(id)
+
+        if(!isNaN(id)){
+            const enquiry = await models.enquiry.findOne({ where:{id} })
+            if(enquiry == null)
+                throw {message:"No enquiry with provided id"}
+            return res.status(200).send({success:true, enquiry})
+        }
+        else
+            return res.status(500).send({error: true, message: "Invalid enquiry id"})
+
+    }
+    catch(err){
+        console.log(err)
+        return res.status(500).send({error: true, message: err.message})
+    }
+}
 module.exports = {
     list,
+    getDetailedEnquiry,
 }
