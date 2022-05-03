9bafa975bafd735e3294a36ae52ad2ea1145a08c
Saurabh Jindal <saurabh@careervira.com>
2022-05-03 16:12:27 +0530

    error text of rate limiter library

--- src/services/v1/middleware/rateLimiter.js
+++ src/services/v1/middleware/rateLimiter.js
@@ -4,7 +4,7 @@ const rateLimiter = rateLimit({
     // windowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
     windowMs:  15 * 60 * 1000, // 24 hrs in milliseconds
     max: 100,
-    message: 'You have exceeded the 100 requests in 24 hrs limit!', 
+    message: 'You have exceeded the 100 requests in 15 minutes limit!', 
     headers: true,
 });
 
