c53200d65bc47c02531fc0483b6d26652e27c2d6
Saurabh Jindal <saurabh@careervira.com>
2022-05-03 16:08:57 +0530

    changed rateLimiter settings 100 req. in 15 minutes

--- src/services/v1/middleware/rateLimiter.js
+++ src/services/v1/middleware/rateLimiter.js
@@ -2,8 +2,8 @@ const rateLimit =  require('express-rate-limit');
 
 const rateLimiter = rateLimit({
     // windowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
-    windowMs:  2 * 60 * 1000, // 24 hrs in milliseconds
-    max: 5,
+    windowMs:  15 * 60 * 1000, // 24 hrs in milliseconds
+    max: 100,
     message: 'You have exceeded the 100 requests in 24 hrs limit!', 
     headers: true,
 });
