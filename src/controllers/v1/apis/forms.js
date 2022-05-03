aa67205cdb77d49a0af05f5ca649cf3b3b3dbe53
Saurabh Jindal <saurabh@careervira.com>
2022-05-03 15:58:44 +0530

    added rate limiter middleware

--- src/controllers/v1/apis/forms.js
+++ src/controllers/v1/apis/forms.js
@@ -1,11 +1,12 @@
 const  express = require("express");
 const forms = require("../../../services/v1/forms/forms");
 const injectTokenPayload = require("../../../services/v1/middleware/injectTokenPayload");
+const rateLimiter = require('../../../services/v1/middleware/rateLimiter')
 
 const router = express.Router()
 
 router.post('/get-form-values',injectTokenPayload, forms.getFormValues)
-router.post('/submit-forms', injectTokenPayload, forms.submitForm)
+router.post('/submit-forms', rateLimiter, injectTokenPayload, forms.submitForm)
 router.post('/default-values', forms.getDefaultValues)
 
 module.exports = router
