4d96fce087f9da945c33b989d81d5d117a98cf8b
Sudhir <sudhir@ajency.in>
2022-09-19 09:25:18 +0530

    key change in geo location api

--- src/api/services/geoIpService.js
+++ src/api/services/geoIpService.js
@@ -9,7 +9,7 @@ const regionToCurrency = {
         getIpDetails: async(ip) => {
 
             try {
-                let details = await models.sequelize.query(`select latitude, longitude, accuracy_radius, continent_name, country_name,country_iso_code, subdivision_1_name, city_name
+                let details = await models.sequelize.query(`select latitude, longitude, accuracy_radius, continent_name, country_name,country_iso_code as country_code, subdivision_1_name, city_name
                 from geoip2_networks net
                 left join geoip2_locations location on (
                   net.geoname_id = location.geoname_id
