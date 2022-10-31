c22dbcea7a76bd09f622dec82a217dac13f9ca36
vineshraut <vinesh@ajency.in>
2022-10-31 15:58:53 +0530

    added caching for related articles for news

--- src/api/services/recommendationArticles.js
+++ src/api/services/recommendationArticles.js
@@ -2,8 +2,8 @@
 
 const elasticService = require("./elasticService");
 const RecommendationService = require("../services/recommendationService");
-const redisConnection = require('../../services/v1/redis');
-const RedisConnection = new redisConnection();
+const RedisConnection = require('../../services/v1/redis');
+const redisConnection = new RedisConnection();
 const recommendationService = new RecommendationService();
 
 
@@ -25,65 +25,63 @@ module.exports = class recommendationArticles{
             const newsId = req.query.newsId.toString();
             const { page = 1, limit = 6 , section} = req.query;
             const offset = (page - 1) * limit;
-            let articles = [];
-            let cacheKey = `Recommendation-Article-For-${newsId}-${section}`;
-            let cachedData = await RedisConnection.getValuesSync(cacheKey);
+            let articles = [], cacheKey = `Recommendation-Article-For-${newsId}-${section}`;
+            let cachedData = await redisConnection.getValuesSync(cacheKey);
 
             if (cachedData.noCacheData != true)
-                articles = cachedData;
-            else {
-                //priority 1 category list; a => field name in article index; b => field name in news index
-                let priorityList1 = [{a:'skills.keyword', b:'skills.keyword'}, {a:'topics.keyword', b:'topics.keyword'},
-                            {a:'sub_categories.keyword', b:'sub_categories.keyword'}, {a:'categories.keyword',b:'categories.keyword'}];
-                let priorityList2 = [{ a:'author_id', b:'authors'}, { a:'partners', b:'partners'}, {a:'regions.keyword', b:'regions.keyword'}];
+                return { success: true, message: "list fetched successfully", data:{ list:cachedData, mlList:[], show:'logic'} };
 
-                const relationData = {
-                    index: "news",
-                    id: newsId
-                }
+            //priority 1 category list; a => field name in article index; b => field name in news index
+            let priorityList1 = [{a:'skills.keyword', b:'skills.keyword'}, {a:'topics.keyword', b:'topics.keyword'},
+                        {a:'sub_categories.keyword', b:'sub_categories.keyword'}, {a:'categories.keyword',b:'categories.keyword'}];
+            let priorityList2 = [{ a:'author_id', b:'authors'}, { a:'partners', b:'partners'}, {a:'regions.keyword', b:'regions.keyword'}];
 
-                let esQuery = {
-                    "bool": { "filter": [ { "term": { "status.keyword": "published" } } ] }
-                }
-                if (section) {
-                    esQuery.bool.filter.push(
-                        { "term": { "section_name.keyword": section } }
-                    );
-    
-                }
-
-                function buildQueryTerms(key, i) {
-                    let termQuery = { "terms": {} };
-                    termQuery.terms[key.a] = { ...relationData, "path": key.b };
-                    termQuery.terms.boost = 5 - (i * 0.1);
-                    return termQuery;
-                }
-
-                esQuery.bool.should = [{
-                    bool: {
-                        boost: 1000,
-                        should: priorityList1.map(buildQueryTerms)
-                    }
-                }];
-
-                esQuery.bool.should.push({
-                    bool: {
-                        boost: 10,
-                        should: priorityList2.map(buildQueryTerms)
-                    }
-                })
-
-                let result = await elasticService.search("article", esQuery, { from: offset, size: limit ,_source: articleFields});
-
-                if (result && result.hits.length > 0) {
-                    for (let hit of result.hits) {
-                        let article = await recommendationService.generateArticleFinalResponse(hit._source);
-                        articles.push(article);
-                    }
-                }
-                await RedisConnection.set(cacheKey, articles)
-                RedisConnection.expire(cacheKey, process.env.CACHE_EXPIRE_ARTICLE_RECOMMENDATION); 
+            const relationData = {
+                index: "news",
+                id: newsId
             }
+
+            let esQuery = {
+                "bool": { "filter": [ { "term": { "status.keyword": "published" } } ] }
+            }
+            if (section) {
+                esQuery.bool.filter.push(
+                    { "term": { "section_name.keyword": section } }
+                );
+
+            }
+
+            function buildQueryTerms(key, i) {
+                let termQuery = { "terms": {} };
+                termQuery.terms[key.a] = { ...relationData, "path": key.b };
+                termQuery.terms.boost = 5 - (i * 0.1);
+                return termQuery;
+            }
+
+            esQuery.bool.should = [{
+                bool: {
+                    boost: 1000,
+                    should: priorityList1.map(buildQueryTerms)
+                }
+            }];
+
+            esQuery.bool.should.push({
+                bool: {
+                    boost: 10,
+                    should: priorityList2.map(buildQueryTerms)
+                }
+            })
+
+            let result = await elasticService.search("article", esQuery, { from: offset, size: limit ,_source: articleFields});
+
+            if (result && result.hits.length > 0) {
+                for (let hit of result.hits) {
+                    let article = await recommendationService.generateArticleFinalResponse(hit._source);
+                    articles.push(article);
+                }
+            }
+            await redisConnection.set(cacheKey, articles, process.env.CACHE_EXPIRE_ARTICLE_RECOMMENDATION || 360)
+
            
             return { success: true, message: "list fetched successfully", data:{ list:articles, mlList:[], show:'logic'} };
             
